c601e3f53e594bf3bc08432330cfa00549447e02
MakoriDev <masterprowriters@gmail.com>
2023-03-19 12:29:32 -0700

    First task in C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,26 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
+
 /**
- * main-main function
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
  *
- * Return: always value
- */
+ * Return: Always 0.
+*/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 
 	if (n > 0)
-		printf("%d is positive", n);
-	else if (n == 0)
-		printf("%d is zero", n);
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 	else
-		printf("%d is negative", n);
-	printf("\n");
-
-
-
+		printf("%d is zero\n", n);
 
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+This is ALX class where I am insipiring to be a full stack engineer and make a sound living as opposed to risking my future
+0x01. C - Variables, if, else, while
