38e155744729860be45c6532625fd470d4e20953
MakoriDev <masterprowriters@gmail.com>
2023-03-28 01:47:27 -0700

    task 12

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,33 +1,48 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
-#include main.h
-int main()
+/**
+ * checksum - executes checksum
+ * @s: input char
+ * Return: checksum
+ */
+unsigned long checksum(char *s)
 {
-    int length, i;
-    printf("Enter the length of the password: ");
-    scanf("%d", &length);
-    char password[length];
-    char uppercase[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char lowercase[] = "abcdefghijklmnopqrstuvwxyz";
-    char numbers[] = "0123456789";
-    char symbols[] = "!@#$%^&*()_+-={}[];\':\",./<>?\\|";
-    char all[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-={}[];\':\",./<>?\\|";
+unsigned long sum = 0;
+while (*s != 0)
+{
+	sum += *s;
+	s++;
+}
+return (sum);
+}
+/**
+ * main - prints password for crakme
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQSTUVWXYZ";
+	char s[33];
+	unsigned long sum;
+	int i, flag = 0;
 
-    srand(time(NULL));
-    printf("Generated Password: ");
-    for(i=0; i<length; i++)
-    {
-        if(i==0)
-            password[i] = uppercase[rand() % 26];
-        else if(i==1)
-            password[i] = lowercase[rand() % 26];
-        else if(i==2)
-            password[i] = symbols[rand() % 28];
-        else
-            password[i] = all[rand() % 72];
-        printf("%c", password[i]);
-    }
-    printf("\n");
-    return 0;
+	srand(time(NULL));
+	while (flag == 0)
+	{
+		for (i = 0; i < 33; i++)
+		{
+			s[i] = alpha[rand() % (sizeof(alpha) - 1)];
+		}
+		s[i] = '\0';
+		sum = checksum(s);
+		if (sum == 2772)
+		{
+			flag = 1;
+			printf("%s", s);
+		}
+	}
+return (0);
 }
