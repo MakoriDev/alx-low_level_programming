7284c6653fd2abe0ed334ea973fae996d0f4f802
MakoriDev <masterprowriters@gmail.com>
2023-03-28 01:45:14 -0700

    advanced tasks

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,33 @@
-#import "main.h"
-/**
- * main -
- * Return:
- */
-int main(void)
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include main.h
+int main()
 {
-	return (0);
+    int length, i;
+    printf("Enter the length of the password: ");
+    scanf("%d", &length);
+    char password[length];
+    char uppercase[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char lowercase[] = "abcdefghijklmnopqrstuvwxyz";
+    char numbers[] = "0123456789";
+    char symbols[] = "!@#$%^&*()_+-={}[];\':\",./<>?\\|";
+    char all[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-={}[];\':\",./<>?\\|";
+
+    srand(time(NULL));
+    printf("Generated Password: ");
+    for(i=0; i<length; i++)
+    {
+        if(i==0)
+            password[i] = uppercase[rand() % 26];
+        else if(i==1)
+            password[i] = lowercase[rand() % 26];
+        else if(i==2)
+            password[i] = symbols[rand() % 28];
+        else
+            password[i] = all[rand() % 72];
+        printf("%c", password[i]);
+    }
+    printf("\n");
+    return 0;
 }
