3aca9baa8020c42330c010b5e64bdb7f219b6f07
MakoriDev <masterprowriters@gmail.com>
2023-03-28 01:51:01 -0700

    revision

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,31 +1,25 @@
 #include "main.h"
 /**
- * rev_string - reverses a string
- * @s: holi
- *
- */
+  * rev_string - prints string
+  * @s: input string
+  * Return: no return
+**/
 void rev_string(char *s)
 {
-	int i;
-	int j;
-	int temp;
-	int length;
-
-	while (s[i] != 0)
-	{
-		i++;
-	}
-
-	length = i;
-	i = 0;
-	j = length - 1;
-
-	while (i < j)
-	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
-		i++;
-		j--;
-	}
+int i = 0, j = 0;
+char aux;
+while (s[i] != '\0')
+{
+i++;
+}
+while (i > 0)
+{
+for (j = 0; j < (i - 1); j++)
+{
+aux = s[j + 1];
+s[j + 1] =  s[j];
+s[j] = aux;
+}
+i--;
+}
 }
