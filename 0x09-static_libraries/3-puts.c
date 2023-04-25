83a3c8aaf1307d3f8ec12f8a64656785a39d62a8
MakoriDev <masterprowriters@gmail.com>
2023-04-25 07:44:33 -0700

    advanced

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "main.h"
+
+void _puts(char *str)
+{
+    /* implementation of _puts function */
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
+
